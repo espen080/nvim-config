42df07e703f33a9086e55edf993d36026f219f2d
Espen Klungland <espen.klungland@aider.no>
2025-03-18 08:31:35 +0100

    changing color scheme

--- init.lua
+++ init.lua
@@ -922,7 +922,9 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'unokai'
+      vim.api.nvim_set_hl(0, 'Normal', { guibg = NONE, ctermbg = NONE })
       LineNumberColors()
     end,
   },
