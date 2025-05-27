9a3aa359bb6e7212b9114195f07c7056c202f3eb
Espen Klungland <espen.klungland@kartverket.no>
2025-05-27 09:36:05 +0200

    added column indicator

--- init.lua
+++ init.lua
@@ -111,6 +111,9 @@ vim.opt.mouse = ''
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- Show cloumn indicator at 80 characters
+vim.opt.colorcolumn = '80'
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
