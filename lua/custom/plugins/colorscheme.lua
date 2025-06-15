6c39a3a495fdae68573873ec30190f5ef5440505
Espen Klungland <espen.klungland@gmail.com>
2025-06-15 12:47:54 +0200

    changed colorscheme to rose-pine

---
+++ lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  'rose-pine/neovim',
+  priority = 1000, -- Make sure to load this plugin first.
+  name = 'rose-pine',
+  config = function()
+    require('rose-pine').setup {
+      variant = 'main',
+      styles = {
+        transparency = true,
+      },
+      highlight_groups = {
+        TelescopeBorder = { fg = 'highlight_high', bg = 'none' },
+        TelescopeNormal = { bg = 'none' },
+        TelescopePromptNormal = { bg = 'none' },
+        TelescopeResultsNormal = { fg = 'subtle', bg = 'none' },
+        TelescopeSelection = { fg = 'text', bg = 'none' },
+        TelescopeSelectionCaret = { fg = 'rose', bg = 'none' },
+      },
+    }
+    vim.cmd 'colorscheme rose-pine'
+  end,
+}
