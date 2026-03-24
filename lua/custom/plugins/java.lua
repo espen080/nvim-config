adf7d5ae15eabc988e052bbbc8df3eeee3cd5ee2
Espen Klungland <espen.klungland@kartverket.no>
2026-03-24 10:16:27 +0100

    java indentation

--- lua/custom/plugins/java.lua
+++ lua/custom/plugins/java.lua
@@ -4,6 +4,16 @@ return {
     config = function()
       require('java').setup()
       vim.lsp.enable 'jdtls'
+
+      -- Set java indentation
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'java',
+        callback = function()
+          vim.opt_local.tabstop = 4
+          vim.opt_local.shiftwidth = 4
+          vim.opt_local.expandtab = true
+        end,
+      })
     end,
   },
 }
