52e5c180e5598aa2e907f8ad6158fbbf589835e8
Espen Klungland <espen.klungland@kartverket.no>
2026-06-24 14:56:37 +0200

    upgrade telescope

--- lua/custom/plugins/venvselector.lua
+++ lua/custom/plugins/venvselector.lua
@@ -9,7 +9,7 @@ return {
     'neovim/nvim-lspconfig',
     'mfussenegger/nvim-dap',
     'mfussenegger/nvim-dap-python', --optional
-    { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+    { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
   },
   ft = 'python', -- Load when opening Python files
   keys = {
