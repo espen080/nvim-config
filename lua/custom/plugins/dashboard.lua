2ebabede52febd90776b9a84f59dc463e6527c99
Espen Klungland <espen.klungland@kartverket.no>
2026-06-25 08:58:37 +0200

    use chafa to render logo

--- lua/custom/plugins/dashboard.lua
+++ lua/custom/plugins/dashboard.lua
@@ -7,12 +7,17 @@ return {
       dashboard = {
         enabled = true,
         sections = {
-          { section = 'header' },
-          { section = 'keys', gap = 1, padding = 1 },
-          { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
-          { pane = 2, icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          -- { section = 'header' },
           {
-            pane = 2,
+            section = 'terminal',
+            cmd = 'chafa ~/.config/logo.png --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1',
+            height = 17,
+            padding = 1,
+          },
+          { section = 'keys', gap = 1, padding = 1 },
+          { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          {
             icon = ' ',
             title = 'Git Status',
             section = 'terminal',
